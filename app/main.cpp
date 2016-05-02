e7e7aa79a6dc3c58701224d2baef85ec2af3a113
Alexander Aprelev <aam@google.com>
2016-05-02 09:30:43 -0700

    License and copyright.

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ */
+
 #include "SkCanvas.h"
 #include "SkDocument.h"
 #include "SkFontMgr.h"
